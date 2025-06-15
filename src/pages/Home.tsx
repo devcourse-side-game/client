4a305207e3f16560e923a743eed8ca15aeb2a5f3
Sori <chan8919@naver.com>
2025-06-15 17:26:46 +0900

    <refactor> 테스트용 페이지 헤더, 푸터 생성 및 임시 메인 페이지 구성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import TestHeader from '../testSori/TestHeader';
+import TestFooter from '../testSori/TestFooter';
+import PartyBoard from '../components/PartyBoard';
 
 function Home() {
-	return <div className='Home'>Home Page</div>;
+	return (
+		<div className='Home'>
+			<TestHeader></TestHeader>
+			<PartyBoard></PartyBoard>
+			<TestFooter></TestFooter>
+		</div>
+	);
 }
 
 export default Home;
