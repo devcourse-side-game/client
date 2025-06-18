c0178f997481570a1473a17a998e90afc54a1ff1
Sori <chan8919@naver.com>
2025-06-18 20:17:48 +0900

    React-Query 사용을 위한 queryClient 생성

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 5, // 5초동안 fresh 상태 유지
+			gcTime: 1000 * 60 * 5, // 5분 동안 캐시 유지
+			refetchOnWindowFocus: false, // 윈도우 포커스시 자동 재요청 비활성화 (개발용)
+		},
+	},
+});
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
 
