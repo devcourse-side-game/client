71126a54b93fb3376bb8c8e40fa382ae2cedd507
Sori <chan8919@naver.com>
2025-06-18 17:35:49 +0900

    임시수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import PartyList from './components/PartyList';
+import Home from './pages/Home';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<PartyList></PartyList>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		// <div className='App'>
+		// 	<header className='App-header'>
+		// 		<PartyList></PartyList>
+		// 		<img src={logo} className='App-logo' alt='logo' />
+		// 		<p>
+		// 			Edit <code>src/App.tsx</code> and save to reload.
+		// 		</p>
+		// 		<a
+		// 			className='App-link'
+		// 			href='https://reactjs.org'
+		// 			target='_blank'
+		// 			rel='noopener noreferrer'
+		// 		>
+		// 			Learn React
+		// 		</a>
+		// 	</header>
+		// </div>
+		<Home></Home>
 	);
 }
 
