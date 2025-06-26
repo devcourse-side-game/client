8bf3fd571ede073331e198e1211e593f3969bb36
Sori <chan8919@naver.com>
2025-06-26 18:12:17 +0900

    <feature> Home에서 Header 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import TestHeader from '../testSori/TestHeader';
-import TestFooter from '../testSori/TestFooter';
 import PartyBoard from '../components/Party/PartyBoard';
 import { ModalProvider } from '../contexts/ModalProvider';
 import PartyGlobalModal from '../components/Party/PartyModal/PartyGlobalModal';
@@ -9,9 +7,8 @@ function Home() {
 	return (
 		<div className='Home'>
 			<ModalProvider>
-				<TestHeader></TestHeader>
 				<PartyBoard></PartyBoard>
-				<TestFooter></TestFooter>
+
 				<PartyGlobalModal />
 			</ModalProvider>
 		</div>
