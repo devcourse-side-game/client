1c265d6ccfc2c82d0350c96433fd1557363b261c
danbe8080 <danbe8080@gmail.com>
2025-07-07 18:44:32 +0900

    <feat> 로그인 후 로그인&회원가입 페이지 접근 불가능 처리

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
 import Signup from '../pages/Signup';
 import Login from '../pages/Login';
@@ -6,13 +6,16 @@ import MyPage from '../pages/MyPage';
 import InfoMangement from '../components/user/InfoMangement';
 import GameMangement from '../components/user/GameMangement';
 import ThemeMangement from '../components/user/ThemeMangement';
+import { RootState } from '../stores';
+import { useSelector } from 'react-redux';
 
 function AppRoutes() {
+	const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
-			<Route path='/login' element={<Login />} />
-			<Route path='/signup' element={<Signup />} />
+			<Route path='/login' element={isLoggedIn ? <Navigate to='/' replace /> : <Login />} />
+			<Route path='/signup' element={isLoggedIn ? <Navigate to='/' replace /> : <Signup />} />
 			<Route path='/mypage' element={<MyPage />}>
 				<Route index element={<InfoMangement />} />
 				<Route path='game' element={<GameMangement />} />
